30e14c440f027b3355083762a0c4f13de0a19da9
Andrew Lichtenhan <arlichte@gmail.com>
2015-08-27 10:47:11 -0400

    first commit, but it failed

---
+++ photobomb/photobomb.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'unirest'
+
+get '/' do
+	@title = 'Photos'
+	@photos = recent_photos
+	erb :all_photos
+end
+
+def recent_photos (hashtag = "corvette", resolution = "thumbnail")
+	@photos = []
+	instagram_response = "https://api.instagram.com/v1/tags/#{hashtag}/media/recent?client_id=[e390deda948f410b8a0ec4970826bb70]"		
+	response = Unirest.get(instagram_response)
+	photos_array = response.body["data"]
+
+	photos_array.each do |photo|
+		image = photo["images"][resolution]["url"]
+		@photos << image
+	end
+
+	@photos
+end
+
+get '/:resolution/:hashtag' do
+	@title = 'Photos'
+	@photos = recent_photos(params[:hashtag],params[:resolution])
+	erb :all_photos
+end
